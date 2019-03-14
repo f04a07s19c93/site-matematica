aedb32e7fe389b08b4d86552f61be65666e45b2a
Filipe Augusto Santos Chaves <f04a07s19c93@gmail.com>
2019-03-13 22:54:08 -0300

    adicionado as funcoes para multiplicacao, mdc e mmc

--- js/main.js
+++ js/main.js
@@ -4,19 +4,35 @@ $(function(){
         for(x =1;x<=quant;x++){
                     input_x = $("<input>")
                     tr = $("<tr>");
-                    tr.append("<td>"+x+"</td>"+
-                    "<td>"+"1+1+33+3333"+"</td>");
-                    td = $("<td>");
-                    inputText = $("<input type='text' >")
-                    inputButton = $("<input type='button' class='btn btn-success btncorrecao' value='ok'>")
+                var selecaoDificuldade =function (){
+                                        if(dificul=="facil"){                                           
+                                         return facil(assunt,x);
+                                        }if(dificul=="media"){
+                                           return     medio(assunt,x);
+                                        }if(dificul=="dificil"){
+                                              return    dificil(assunt,x);}
+                                        }
+
+                    tdExercicio =$("<td>"+selecaoDificuldade() +"</td>");//conta
+                    tdDificuldade = $("<td>"+ dificul +"</td>");
+                    
+                    tr.append("<td>"+x+"</td>"); 
+                    tr.append(tdDificuldade)                   
+                    tr.append(tdExercicio);    
+                    tr.append( "<td>"+"certo ou errado"+"</td>")              
+                     $("#corpoTabela").append(tr);
+                   /* td2 = $("<td>");
+                    inputText = $("<input type='text' >")//campo texto
+                    inputButton = $("<input type='button' class='btn btn-success btncorrecao' value='ok'>")//campo botao
                             inputButton.click(function(){
                                 acaoContas();
                             });
-                    td.append(inputText);
-                    td.append(inputButton);
-                    tr.append(td);
-                    tr.append( "<td>"+"certo ou errado"+"</td>")   
-                     $("#corpoTabela").append(tr);
+                    td2.append(inputText);
+                    td2.append(inputButton);
+                    tr.append(td2);*/
+                 
+                    
+                    
                     }
             }
 
@@ -33,6 +49,91 @@ $("#acao").click(function(){
 function acaoContas(){
     alert('aqui vai rolar as contas!!')
 }
+/* dificuldade*/
+function facil(assunto,contador){
+        if(assunto=="Multiplicação"){
+               return  multiplicacao_facil(x);
+        }if(assunto=="MMC"){
+                return mmc_facil(x);
+        }if(assunto=="MDC"){
+                return mdc_facil(x);    
+        }
+} 
+function medio (assunto,contador){   
+        if(assunto=="Multiplicação"){
+                return    multiplicacao_medio(x)
+       }if(assunto=="MMC"){
+               return mmc_medio(x);
+       }if(assunto=="MDC"){
+        return mdc_medio(x);    
+}
+}
+function dificil(assunto,contatdor){
+       if(assunto=="Multiplicação"){
+                 return   multiplicacao_dificil(x)        
+       }if(assunto=="MMC"){
+        return mmc_dificil(x);
+       } if(assunto=="MDC"){
+        return mdc_dificil(x);    
+}
+}
+/*formulas */
+function multiplicacao_facil (x){
+               /*multiplicação simples com 2 valores e numeros inteiros positivos*/ 
+       if (x<=4){
+                return (Math.round(Math.random() * 100  )) + " × " +(Math.round(Math.random() * 100  ))+ "="
+        } if(x >= 5 && x < 10 ){
+                return (Math.round(Math.random() * 100  )) + " × " +(Math.round(Math.random() * 100  )) + " × " + (Math.round(Math.random() * 100  ))+ "=";        
+        }
+       if (x >=10 ){
+                return (Math.round(Math.random() * 100  )) + " × " +(Math.round(Math.random() * 100  )) + " × " +
+                 (Math.round(Math.random() * 100  ))+ " × " +      (Math.round(Math.random() * 100  ))+ "=";
+        }
+}
+/*MULTIPLICAÇÃO*/ 
+function multiplicacao_medio (x){
+        /*multiplicação com numeros negativos*/
+if (x<=3){
+         return ((Math.round(Math.random() * 100))-(Math.round(Math.random() * 100 ))) + " × " +((Math.round(Math.random() * 100))-(Math.round(Math.random() * 100 )))+ "="
+ } if(x >= 4 && x < 10 ){
+         return ((Math.round(Math.random() * 100))-(Math.round(Math.random() * 100 ))) + " × " +((Math.round(Math.random() * 100))-(Math.round(Math.random() * 100 ))) + " × " + ((Math.round(Math.random() * 100))-(Math.round(Math.random() * 100 )))+ 
+         " × " + ((Math.round(Math.random() * 100))-(Math.round(Math.random() * 100 )))+ " =";        
+ }
+if (x >=10 ){
+         return ((Math.round(Math.random() * 100))-(Math.round(Math.random() * 100 ))) + " × " +((Math.round(Math.random() * 100))-(Math.round(Math.random() * 100 ))) + " × " +
+         ((Math.round(Math.random() * 100))-(Math.round(Math.random() * 100 )))+ " × " +     ((Math.round(Math.random() * 100))-(Math.round(Math.random() * 100 )))+
+         " × " +     ((Math.round(Math.random() * 100))-(Math.round(Math.random() * 100 )))+ " =";
+ }
+}
+function multiplicacao_dificil(x){
+        return (parseFloat((Math.random() * 100)- (Math.random() * 100)).toFixed(2)) + " × " +
+        (parseFloat((Math.random() * 100)- (Math.random() * 100)).toFixed(2))+" ="
+}
+
+/*MMC*/ 
+function mmc_facil(x){
+return "MMC( " +(Math.round(Math.random() * 100))+" , "+(Math.round(Math.random() * 100)) +") ="
+}
+function mmc_medio(x){
+        return "MMC( " +(Math.round(Math.random() * 500))+" , "+(Math.round(Math.random() * 500)) + " , "+
+        +(Math.round(Math.random() * 500))+ ") ="
+}
+function mmc_dificil(x){
+   return "MMC( " +(Math.round(Math.random() * 1000))+" , "+(Math.round(Math.random() * 1000))   + " , "+
+        (Math.round(Math.random() * 1000))+  ") ="
+}
+/*MDC*/ 
+function mdc_facil(x){
+        return "MDC( " +(Math.round(Math.random() * 100+50))+" , "+(Math.round(Math.random() * 100+50)) +") ="
+        }
+function mdc_medio(x){
+                return "MDC( " +(Math.round(Math.random() * 500+50))+" , "+(Math.round(Math.random() * 500+50)) + " , "+
+                +(Math.round(Math.random() * 500))+ ") ="
+        }
+function mdc_dificil(x){
+           return "MDC( " +(Math.round(Math.random() * 1000+50))+" , "+(Math.round(Math.random() * 1000+50))   + " , "+
+                (Math.round(Math.random() * 1000))+  ") ="
+        }
 
 
 
